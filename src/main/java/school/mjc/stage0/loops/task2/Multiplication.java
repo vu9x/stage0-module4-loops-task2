c141e15c0e3d369248c879a1755c5b091cf3010f
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 11:16:25 +0300

    Adding: Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int counter = 0;
+        int positive = multiplyByAndToInclusive >=0 ? multiplyByAndToInclusive: multiplyByAndToInclusive * (-1);
+
+        while (counter <= positive) {
+            System.out.println(counter * multiplyByAndToInclusive);
+            counter++;
+        }
     }
 }
