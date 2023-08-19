ae346d5d710587de073e73a65f2faf137db7ac04
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 12:10:03 +0300

    Adding WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
-    public void buildPhrase(char... chars) {
-
+    public static void buildPhrase(char... chars) {
+        System.out.println(chars);
     }
 }
