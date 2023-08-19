09b7db55a97aa9b122134289e77065215799bb55
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 11:11:57 +0300

    Adding EvenNumbersPrinter.java

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        int isEven = 0;
+
+        while (isEven <= printTillInclusive) {
+            if (isEven % 2 == 0) {
+                System.out.println(isEven);
+            }
+            isEven++;
+        }
     }
 }
