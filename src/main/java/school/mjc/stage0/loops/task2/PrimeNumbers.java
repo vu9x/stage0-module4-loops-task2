67127ce92732d5fb909cd1f53cc2bbb3e92306ca
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 11:37:58 +0300

    Adding: PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+
+        while (counter <= printToInclusive) {
+            boolean isPrime = true;
+            int num = 2;
+
+            //Перебираем числа. Если есть делитель, кроме 1 и counter, значит число составное
+            while (num < counter) {
+                if (counter % num == 0) {
+                    isPrime = false;
+                }
+                num++;
+            }
+
+            //Если число простое, печатаем
+            if (isPrime) {
+                System.out.println(counter);
+            }
+
+            counter++;
+        }
     }
 }
