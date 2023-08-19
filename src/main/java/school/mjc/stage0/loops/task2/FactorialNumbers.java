36ed09b984943cd02c0ad2bcc9b012bd02e61854
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 11:44:10 +0300

    Adding: FactorialNumber.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 0;
+        int result = 0;
 
+        while (counter <= printToInclusive) {
+            if (counter == 0 || counter == 1) {
+                result = 1;
+            } else {
+                result *= counter;
+            }
+
+            System.out.println(result);
+            counter++;
+        }
     }
 }
