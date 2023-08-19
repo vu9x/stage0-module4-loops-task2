479bd5c4b5764843aafa21213725013cac788fac
Vu <nguyentuanvu9x@gmail.com>
2023-08-19 11:56:08 +0300

    Adding: PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int counter = 0;
+        int value = 1;
 
+        if (power < 0) {
+            System.out.println("too much power");
+        }
+
+        while (counter <= power) {
+            if (counter != 0) {
+                value *= 2;
+            }
+            System.out.println(value);
+            counter++;
+        }
     }
 }
